2b01da3f4a712e3fa63ec9ca19e34f00d434b406
LeeKangWon <wangchozza1@gmail.com>
2016-08-05 18:48:38 +0900

    8번째 과제

--- C Programming/2016 SummerVacation/HW8/HW8-5.c
+++ C Programming/2016 SummerVacation/HW8/HW8-5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-void delete_space(char *);
+void delete_number(char *);
 
 int main() 
 {
@@ -9,17 +9,45 @@ int main()
 
 	gets_s(str1,sizeof(str1));
 
-	delete_space(str1);
+	delete_number(str1);
+
+	return 0;
 }
 
-void delete_space(char *str1)
+void delete_number(char *str1)
 {
 	int i = 0;
-	for(i=0; str1[i]!=NULL; i++)
+	int j = 0;
+	int number = 0;
+
+	while(1)
 	{
-		if (str1[i] == ' ')
+		printf("삭제할 문자열의 번호를 입력하시오 : ");
+		scanf("%d", &number);
+
+		if(number <= 0)
+		{
+			printf("음수 또는 0을 입력하셨습니다. 다시 입력하세요\n");
 			continue;
-		printf("%c", str1[i]);
+		}
+		
+		if(number > strlen(str1))
+			number = strlen(str1);
+
+		for(i=number-1; i<strlen(str1); i++)
+
+		{
+			str1[i] = str1[i+1];
+		}
+		printf("삭제하고 남은 문자열 : %s\n", str1);
+		printf("삭제하고 남은 문자열의 길이 : %d\n", strlen(str1));
+
+		if(strlen(str1) == 1)
+		{
+			printf("남은 문자열이 1개 입니다. 종료합니다.\n");
+			break;
+		}
 	}
+
 }
 
